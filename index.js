2318d7eb247e1ae00076d09d21ea1835ae70aa6b
Matthew Levenstein <matthewlevenstein@gmail.com>
2023-12-22 23:38:10 -0500

    updates

--- index.js
+++ index.js
@@ -2,80 +2,54 @@ const url = require('url')
 const querystring = require('querystring')
 const fs = require('fs')
 const path = require('path')
+const formidable = require('formidable')
+const mimedb = require('./mime.json')
 
-const mimeTypes = {
-  'css'  : 'text/css',
-  'html' : 'text/html',
-  'js'   : 'application/javascript',
-  'png'  : 'image/png',
-  'jpg'  : 'image/jpeg',
-  'svg'  : 'image/svg+xml',
-  'ico'  : 'image/x-icon',
-  'docx' : 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-}
+const mimeTypes = {};
+
+Object.keys(mimedb).forEach((type) => {
+  if (mimedb[type].extensions) {
+    mimedb[type].extensions.forEach((ext) => {
+      mimeTypes[ext] = type;
+    });
+  }
+});
 
 function trimAndSplit (s) {
   if (s[0] === '/') s = s.slice(1)
   if (s[s.length - 1] === '/') s = s.slice(0, s.length - 1)
-
   return s.split('/')
 }
 
 function app (req, res) {
   const method = req.method.toLowerCase()
   const handlers = app.handlers
+
+  if (method === 'get' && app.publicDir) {
+    const p = url.parse(req.url).pathname;
+    const f = path.resolve(process.cwd(), app.publicDir, p.slice(1));
+console.log('!!!!', f)
+    const ext = path.extname(f);
+    if (ext && serveStaticFile(f, res)) return;
+  }
+
   let found = false
   let i = 0
 
-  res.success = (function () {
-    this.status = 200
-    this.end()
-  }).bind(res)
+  res.redirect = function (url) {
+    res.writeHead(301, { 'Location': url })
+    res.end()
+  }
 
-  res.redirect = (function (url) {
-    this.writeHead(301, { 'Location': url })
-    this.end()
-  }).bind(res)
-
-  res.json = (function (status, obj) {
+  res.json = function (status, obj) {
     if (!obj) {
       obj = status
       status = 200
     }
 
-    this.status = status
-    this.setHeader('Content-Type', 'application/json')
-    this.end(JSON.stringify(obj))
-  }).bind(res)
-
-  res.html = (function (status, str) {
-    if (!str) {
-      str = status
-      status = 200
-    }
-
-    this.status = status
-    this.setHeader('Content-Type', 'text/html')
-    this.end(str)
-  }).bind(res)
-
-  res.csv = (function (status, obj) {
-    if (!obj) {
-      obj = status
-      status = 200
-    }
-
-    this.status = status
-
-    this.setHeader('Content-disposition', 'attachment; filename=' + obj.filename)
-    this.setHeader('Content-Type', 'text/csv')
-    this.end(obj.data)
-  }).bind(res)
-
-  // query string
-  if (req.url.indexOf('?') !== -1) {
-    req.query = url.parse(req.url, true).query
-    req.url = req.url.split('?')[0]
+    res.statusCode = status
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify(obj))
   }
 
   function next () {
@@ -84,13 +58,26 @@ function app (req, res) {
         handlers[i].method === '*' ||
         (app.matchRoute(req, handlers[i].route) && method === handlers[i].method)
       ) {
+	found = true
         handlers[i++].handler(req, res, next)
         break
       }
     }
+    if (i === handlers.length && !found) {
+      res.statusCode = 404
+      res.writeHead(404, { 'Content-type': 'text/plain' })
+      res.end('not found')
+    }
   }
 
-  next()
+  if (req.url.indexOf('?') !== -1) {
+    req.query = url.parse(req.url, true).query
+    req.url = req.url.split('?')[0]
+  }
+
+  parseBody(req, function () {
+    next()
+  })
 }
 
 app.matchRoute = function (req, route) {
@@ -162,46 +149,75 @@ function _handle (method, defaultRoute) {
   }
 }
 
-function _handleError (req, res, next) {
-  try {
-    next()
-  } catch (e) {
-    console.log(req.method, req.url, e)
-    res.status = 502
-    res.end('server error')
-  }
-}
+function parseBody (req, cb) {
+  let body = {}
+  let files = []
+  let form = new formidable.IncomingForm()
+  
+  form.onPart = part => {
+    let buf = []
 
-function _logger (req, res, next) {
-  console.log(req.method, req.url)
-  next()
-}
-
-function uid () {
-  let id = ''
-  const s = 'abcdefghijklmnopqrstuvwxyz0123456789'
-  const l = s.length
-
-  for (let i = 0; i < 10; i++) {
-    id += s[Math.floor(Math.random() * l)]
-  }
-
-  return id
-}
-
-function _static (dir, url) {
-  return function (req, res) {
-    const file = url || req.params.url || req.url
-    const spl = file.split('.')
-    const ext = spl[spl.length - 1]
-
-    if (file.indexOf('..') !== -1) {
-      return res.html(404, '<h3>Not found.</h3>')
+    if (!part.filename) {
+      return form.handlePart(part)
     }
 
-    fs.readFile(path.resolve(dir + file), function (err, data) {
+    let name = part.filename
+    let mime = part.mime
+
+    part.on('data', data => {
+      buf.push(data)
+    })
+    
+    part.on('end', () => {
+      buf = Buffer.concat(buf)
+      files.push({
+        name: name,
+        mime: mime,
+        data: buf
+      })
+    })
+  }
+
+  form.on('field', (key, value) => {
+    body[key] = value
+  })
+
+  form.on('end', () => {
+    req.body = body
+    req.files = files
+    cb()
+  })
+
+  form.parse(req)
+}
+
+function serveStaticFile (p, res) {
+  let ext = path.extname(p);
+  if (!ext)
+    return false;
+  ext = ext.slice(1);
+  try {
+    const data = fs.readFileSync(p);
+    const mime = mimeTypes[ext] || 'text/plain';
+    res.writeHead(200, { 'Content-type': `${mime}` });
+    res.end(data);
+    return true;
+  } catch (e) {
+    console.log('XPRSS error', e)
+    return false;
+  }
+}
+
+function _static (dir) {
+  return function (req, res) {
+    const file = req.url
+    const parts = file.split('.')
+    const ext = parts[parts.length - 1]
+    const fullpath = path
+
+    fs.readFile(path.join(dir, + file), function (err, data) {
       if (!err && data) {
-        res.status = 200
+        res.statusCode = 200
         res.setHeader('Content-Type', mimeTypes[ext] || 'text/plain')
         res.end(data)
       } else {
@@ -218,8 +234,10 @@ app.put = _handle('put')
 app.delete = _handle('delete')
 app.use = _handle('*', '*')
 
-app.logger = _logger
-app.error = _handleError
-app.static = _static
+module.exports = function (config) {
+  if (config.publicDir) {
+    app.publicDir = config.publicDir
+  }
+  return app
+}
 
-module.exports = app
